0622b83a0f1bf317cd0b36151ef9a35446a3aa69
Jason Felice <jason.m.felice@gmail.com>
2013-02-22 09:54:02 -0500

    Add some helper functons for tester

--- tester.scm
+++ tester.scm
@@ -17,5 +17,39 @@
 	(string-append dir "/" file))
     files)))
 
-(write (files-in-directory "."))
+(define (filter predicate list)
+  (define results '())
+  (for-each
+    (lambda (element)
+      (if (predicate element)
+	(set! results (cons element results))))
+    list)
+  (reverse results))
+
+(define (file-extension filename)
+
+  (define (last-index-of char str)
+    (let loop ((index (- (string-length str) 1)))
+      (cond
+	((< index 0)
+	 #f)
+	((char=? char (string-ref str index))
+	 index)
+	(else
+	 (loop (- index 1))))))
+
+  (let ((dot-index (last-index-of #\. filename))
+	(slash-index (last-index-of #\/ filename)))
+    (cond
+      ((or (not dot-index)
+	   (and dot-index
+		slash-index
+		(> slash-index dot-index)))
+       "")
+      (else
+       (substring filename dot-index (string-length filename))))))
+
+(define (scheme-file? filename)
+  (member (file-extension filename) '(".scm" ".ss")))
+
 
