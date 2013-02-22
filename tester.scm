f5ea2a68bf00c305757b3c1e316e110c0656a1fe
Jason Felice <jason.m.felice@gmail.com>
2013-02-22 13:18:23 -0500

    Don't need SYSTEM, have ##SHELL-COMMAND

--- tester.scm
+++ tester.scm
@@ -1,13 +1,4 @@
 
-(define (system command)
-  (let ((p (open-process (list 
-			   path: "/bin/sh"
-			   arguments: `("-c" ,command)
-			   stdin-redirection: #f
-			   stdout-redirection: #f
-			   stderr-redirection: #f))))
-    (process-status p)))
-
 (define (files-in-directory dir)
   (let* ((directory-handle (open-directory dir))
 	 (files (read-all directory-handle)))
